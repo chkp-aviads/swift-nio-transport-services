931610b6b793daced18bf93d1f19ef4d6952bfad
Aviad Segev <aviads@checkpoint.com>
2026-08-24 10:45:44 +0300

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
         .library(name: "NIOTransportServices", targets: ["NIOTransportServices"])
     ],
     dependencies: [
-        .package(url: "https://github.com/chkp-aviads/swift-nio.git", from: "2.96.1"),
+        .package(url: "https://github.com/chkp-aviads/swift-nio.git", from: "2.101.4"),
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.2"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
     ],
